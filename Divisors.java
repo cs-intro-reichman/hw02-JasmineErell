308f27164f6b651fb98dc9b0ac63506f2eee1fe7
JasmineErell <jasmine.erell@gmail.com>
2023-12-29 10:13:58 +0200

    fix divisors 1

--- Divisors.java
+++ Divisors.java
@@ -5,7 +5,7 @@ public class Divisors {
 	public static void main (String[] args) {
 		int num = Integer.parseInt(args[0]);
 		int i = 1;
-		while (i<num){
+		while (i<=num){
 			if (num%i == 0){
 				System.out.println(i);
 			}
