feef0ddb1cda83e40c9c5bd5b4c9edce3198a4be
JasmineErell <jasmine.erell@gmail.com>
2023-12-30 08:26:19 +0200

    fix OneOfEachStats1

--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -4,18 +4,18 @@ public class OneOfEachStats1
 {
 	public static void main (String[] args)
 	{
+        int T = Integer.parseInt(args[0]);
         int seed = Integer.parseInt(args[1]);
         Random generator = new Random(seed);
-        int T = Integer.parseInt(args[0]);
+        double gender = generator.nextDouble();// below 0.5 - boy, above 0.5 - girl (50 50 chance)
         int totalKids = 0;
         int with2 = 0;// counts how many famtlies are with 2 children
         int with3 = 0;// counts how many famtlies are with 3 children
-        int with4 = 0;
+        int with4 = 0;// counts how many famtlies are with 4 or more children
         for (int i =0; i < T; i++)
         {
-            double gender = generator.nextDouble();// below 0.5 - boy, above 0.5 - girl (50 50 chance)
             int c = 0;// counts the children in each family
-            if (gender<0.5)
+            if (gender<0.5) // families that starts with boy
             {
                 while (gender<0.5)
                 {
@@ -39,13 +39,13 @@ public class OneOfEachStats1
             }
             else
             {
-                while (gender>0.5)
+                while (gender>0.5)// families that starts with girl
                 {
                     gender = generator.nextDouble();
                     c++;
                 }
                 c++; 
-                totalKids = totalKids + c ;// count thr amount of kids with an extra 1 for the next gender 
+                totalKids = totalKids + c ;// count the amount of kids with an extra 1 for the next gender 
                 if (c>=4)// to count the amount of families with X children
                 {
                     with4 ++;
